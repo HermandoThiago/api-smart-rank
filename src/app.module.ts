6048350a810ab4d3330750c8fb0e2dd29d24f521
HermandoThiago <hermandodev@gmail.com>
2023-06-29 14:49:07 -0300

    feat: mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PlayersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      'mongodb://admin:admin@localhost:27017/smartrank',
+      {},
+    ),
+    PlayersModule,
+  ],
   controllers: [],
 })
 export class AppModule {}
